21760f4eef027f6062919718d62041645b744570
Yu soyoung <sy980911@gmail.com>
2022-05-09 14:00:12 +0900

    ps

---
+++ 1110.py
@@ -0,0 +1,18 @@
+num = int(input()) # n = 12
+
+#initialization
+#variables update -> temp necessity check!
+a_0 = num / 10 # a_0 = 12/10 = 1
+b_0 = num % 10 # b_0 = 12%10 = 2
+a, b, i = a_0, b_0, 0 # a, b = 1, 2
+
+while(1):
+    print(i, a, b)
+    i += 1
+    temp = a # temp = 1
+    a = b # new a = 2
+    b = (temp + b) % 10 # new b = (1+2)%10 = 3
+    if ((a==a_0) and (b==b_0)): # a = 2, b = 3
+        break
+
+print(i)
